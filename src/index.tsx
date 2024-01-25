e4a76e44e5a53d9f9a574db942a1b3ad66b2a726
Matthew U <matthewu@Matthews-MacBook-Pro.local>
2024-01-24 23:21:59 -0500

    2nd commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+
+/*
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -17,3 +21,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+*/
